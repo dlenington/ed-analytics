05d47b049363b0adb13346009e03783a2429e9ef
Dan Lenington <danlening02@gmail.com>
2020-05-25 11:00:31 -0500

    Update CSS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
+import detail from "./pages/detail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="container">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/details" component={detail} />
         </Switch>
       </main>
     </Fragment>
