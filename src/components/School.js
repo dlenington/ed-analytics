db9da82ba764e283031715b0a51962e542a2ac7f
Dan Lenington <danlening02@gmail.com>
2020-05-26 08:56:07 -0500

    Update Home

---
+++ src/components/School.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+export default function School() {
+  return <div></div>;
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default class Home extends Component {
   render() {
-    return <div>Card</div>;
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={12} xs={12}></Grid>
+      </Grid>
+    );
   }
 }
