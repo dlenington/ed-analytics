31dcdb2413b8ab9251ed079ed2d3265cc1b29944
Dan Lenington <danlening02@gmail.com>
2020-05-29 09:12:47 -0500

    CSS Formatting

--- src/components/School.js
+++ src/components/School.js
@@ -13,7 +13,7 @@ export default function School() {
     <Card>
       <CardContent>
         <Typography variant="h5">School Name</Typography>
-        <Grid container alignContent="center">
+        <Grid container>
           <Grid item>
             <Typography>Student Survey</Typography>
             <Typography>5.5</Typography>
