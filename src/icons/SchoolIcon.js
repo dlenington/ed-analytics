60f12d8211d74917b2be47db9ca38b846300467a
Dan Lenington <danlening02@gmail.com>
2020-06-07 11:16:17 -0500

    Create SchoolIcon.js

---
+++ src/icons/SchoolIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { SvgIcon } from "@material-ui/core";
+
+export default function SchoolIcon() {
+  return (
+    <SvgIcon>
+      <path></path>
+    </SvgIcon>
+  );
+}
