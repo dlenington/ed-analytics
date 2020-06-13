6f68a0240a86d81fad10b3e2b580e34283becc4c
Dan Lenington <danlening02@gmail.com>
2020-06-13 08:49:30 -0500

    Update details component

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -8,11 +8,26 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "@material-ui/core";
 
-export default function detail() {
+export default function detail(props) {
+  const {
+    school: { name },
+  } = props;
   return (
     <Grid>
       <Grid item sm />
-      <Grid item sm></Grid>
+      <Grid item sm>
+        <Card>
+          <CardContent>
+            <Grid>
+              <Grid item sm>
+                <Typography variant="h2">{name}</Typography>
+              </Grid>
+              <Grid item sm />
+              <Grid item sm />
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
       <Grid item sm />
     </Grid>
   );
