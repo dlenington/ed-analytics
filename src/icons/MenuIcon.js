588d6bae91aa021971866dd5ebee73d6ed9d0c0e
Dan Lenington <danlening02@gmail.com>
2020-06-25 09:57:09 -0500

    Update menu icon svg position

--- src/icons/MenuIcon.js
+++ src/icons/MenuIcon.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { SvgIcon } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  logo: {
+    overflow: "visible",
+  },
+});
 
 export default function BarChartIcon() {
+  const classes = useStyles();
+
   return (
-    <SvgIcon>
+    <SvgIcon className={classes.logo}>
       <path
         id="path"
         d="M3,19.333H23V17.111H3v2.222Zm0-5.556H23V11.556H3v2.222ZM3,6V8.222H23V6Z"
-        transform="translate(-3 -6)"
+        transform="translate(0 0)"
         fill="#fff"
         fill-rule="evenodd"
         opacity="0.836"
