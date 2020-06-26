b98d3c4104c8fbdc552101c1b52d62f3fb70eb42
Dan Lenington <danlening02@gmail.com>
2020-06-26 09:19:53 -0500

    Fix warnings realted to css class naming convention

--- src/icons/BarChartIcon.js
+++ src/icons/BarChartIcon.js
@@ -5,7 +5,7 @@ export default function BarChartIcon() {
   return (
     <SvgIcon>
       <path
-        class="a"
+        className="a"
         d="M5,9.2H8V19H5ZM10.6,5h2.8V19H10.6Zm5.6,8H19v6H16.2Z"
       ></path>
     </SvgIcon>
