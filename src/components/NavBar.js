492ed5fde998ad17eacbbee692ecbf40c656efb5
Dan Lenington <danlening02@gmail.com>
2020-07-06 09:24:00 -0500

    Update svg logo position and size on nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles, Typography } from "@material-ui/core";
-import Logo from "../icons/Logo";
+import Logo from "../icons/LogoLg";
 import MyMenu from "./MyMenu";
 import { Link } from "react-router-dom";
 
