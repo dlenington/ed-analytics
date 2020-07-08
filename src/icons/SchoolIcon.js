6d55ba41eb69c15342c02d2708979e030dcdb4c1
Dan Lenington <danlening02@gmail.com>
2020-07-08 09:30:04 -0500

    Update school icon color

--- src/icons/SchoolIcon.js
+++ src/icons/SchoolIcon.js
@@ -8,6 +8,7 @@ export default function SchoolIcon() {
         className="b"
         d="M5,13.18v4L12,21l7-3.82v-4L12,17ZM12,3,1,9l11,6,9-4.91V17h2V9Z"
         transform="translate(-1 3)"
+        fill="#707070"
       ></path>
     </SvgIcon>
   );
