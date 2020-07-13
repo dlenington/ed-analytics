9052403ee026ee764a84649e61451682249e83cb
Dan Lenington <danlening02@gmail.com>
2020-07-13 10:42:29 -0500

    Building out profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,9 +4,12 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
-
-const Profile = () => {
-  return <div>Profile</div>;
-};
-
-export default Profile;
+const useStyles = makeStyles({
+  title: {
+    margin: "auto",
+  },
+});
+export default function Profile() {
+  const classes = useStyles();
+  return <h1 className={classes.title}>Profile</h1>;
+}
