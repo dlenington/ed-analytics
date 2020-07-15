3ded121ce1e578790869c1da40079bf6cdd0212d
Dan Lenington <danlening02@gmail.com>
2020-07-15 09:17:09 -0500

    Update profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -18,10 +18,10 @@ export default function Profile() {
         <Grid item sm/>
     <Grid item sm>
 Profile picuture
-
     </Grid>
-    <Grid item sm/>
-      
+    <Grid item sm>
+    Profile Details
+    </Grid>
       </Grid>
     </div>
   );
