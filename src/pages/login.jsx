72a70c48ff959d6d52b53efc33cf9facb81124e5
Dan Lenington <danlening02@gmail.com>
2020-08-16 09:58:53 -0500

    Create login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+    
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Grid container>
+      <Grid item sm>
+    
+      </Grid>
+      <Grid item sm>
+    <Typography>
+      Login Section
+    </Typography>
+      </Grid>
+      <Grid item sm>
+    
+      </Grid>
+    </Grid>
+    );
 };
 
 export default Login;
