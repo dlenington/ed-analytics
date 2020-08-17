d6675ff6e00cd273ee070d20ee136556f9e146a7
Dan Lenington <danlening02@gmail.com>
2020-08-17 09:52:23 -0500

    Update profile page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,15 +1,21 @@
 import React, {Fragment} from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
 import Typography from "@material-ui/core/Typography";
     
+const useStyles = makeStyles((theme) => ({
+ field: {
+   margin: '0px 0px 20px'
+ }
+}));
+
 
 const Login = () => {
+  const classes = useStyles();
   return (
     <Fragment>
-      
-    
     <Grid container>
       <Grid item sm>
     
@@ -30,8 +36,8 @@ const Login = () => {
       </Grid>
       <Grid item sm>
     <Typography>
-      <TextField label="username"/>
-      <TextField label="password"/>
+      <TextField className={classes.field} label="username"/> <br/>
+      <TextField className={classes.field} label="password"/>
     </Typography>
       </Grid>
       <Grid item sm>
