f6388d74d2a2b4948c691db762272c3896e8fde5
Dan Lenington <danlening02@gmail.com>
2020-08-17 09:42:36 -0500

    Update login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
 import Typography from "@material-ui/core/Typography";
     
 
 const Login = () => {
   return (
+    <Fragment>
+      
+    
     <Grid container>
       <Grid item sm>
     
@@ -19,6 +23,24 @@ const Login = () => {
     
       </Grid>
     </Grid>
+
+    <Grid container>
+      <Grid item sm>
+    
+      </Grid>
+      <Grid item sm>
+    <Typography>
+      <TextField label="username"/>
+      <TextField label="password"/>
+    </Typography>
+      </Grid>
+      <Grid item sm>
+    
+      </Grid>
+    </Grid>
+    </Fragment>
+
+
     );
 };
 
